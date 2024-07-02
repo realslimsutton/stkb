3b3a0dc7dfc9a0a78bac37b2486ba4aa2743ed3b
Kieron Sutton <sutton.kieron99@gmail.com>
2024-07-02 02:25:22 +0100

    setup database

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,27 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
+import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
 import postgres from "postgres";
-
+import { sql } from "@vercel/postgres";
 import { env } from "~/env";
 import * as schema from "./schema";
+
+const isProduction = env.NODE_ENV === "production";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
+  conn: postgres.Sql | typeof sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const conn =
+  globalForDb.conn ?? (isProduction ? sql : postgres(env.POSTGRES_URL));
 
-export const db = drizzle(conn, { schema });
+if (!isProduction) {
+  globalForDb.conn = conn;
+}
+
+export const db = isProduction
+  ? drizzleVercel(conn as typeof sql, { schema })
+  : drizzlePostgres(conn as postgres.Sql, { schema });
