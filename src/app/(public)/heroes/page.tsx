b98c570157928da0900f18d5881060660c929767
Kieron Sutton <sutton.kieron99@gmail.com>
2024-07-06 06:07:38 +0100

    wip

--- src/app/(public)/heroes/page.tsx
+++ src/app/(public)/heroes/page.tsx
@@ -2,8 +2,14 @@ import { redirect } from "next/navigation";
 import PageHeader from "../_components/page-header";
 import { HeroProvider } from "./_components/hero-context";
 import HeroTable from "./_components/hero-table";
+import { getUser } from "~/shop-titans/utils";
 
 export default async function Heroes() {
+  const user = await getUser();
+  if (!user) {
+    redirect("/auth/login");
+  }
+
   return (
     <>
       <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] dark:hidden"></div>
